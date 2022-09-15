20374dfc99a943a7d0ec0751f83dbeff7f26c80a
Matheus Hora <matheus11hora@gmail.com>
2022-09-14 21:01:22 -0300

    Final do front end

--- app/lib/src/pages/home/home_tab.dart
+++ app/lib/src/pages/home/home_tab.dart
@@ -5,6 +5,7 @@ import 'package:app/src/config/custom_colors.dart';
 import 'package:app/src/pages/home/components/category_tile.dart';
 import 'package:app/src/pages/home/components/item_title.dart';
 import 'package:app/src/pages/widgets/app_name_widget.dart';
+import 'package:app/src/pages/widgets/custom_shimmer.dart';
 import 'package:app/src/services/utils_services.dart';
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
@@ -18,7 +19,8 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-  String selectedCatrgory = 'Frutas';
+  String selectedCategory = 'Frutas';
+
   GlobalKey<CartIconKey> globalKeyCartItems = GlobalKey<CartIconKey>();
 
   late Function(GlobalKey) runAddToCardAnimation;
@@ -27,11 +29,26 @@ class _HomeTabState extends State<HomeTab> {
     runAddToCardAnimation(gkImage);
   }
 
-  final UtilsServices utilsServices = UtilsServices();
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        setState(() {
+          isLoading = false;
+        });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // App bar
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -39,8 +56,13 @@ class _HomeTabState extends State<HomeTab> {
         title: const AppNameWidget(),
         actions: [
           Padding(
-            padding: const EdgeInsets.only(top: 10, right: 15),
-            child: Badge(
+            padding: const EdgeInsets.only(
+              top: 15,
+              right: 15,
+            ),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
                 badgeColor: CustomColors.customContrastColor,
                 badgeContent: const Text(
                   '2',
@@ -50,15 +72,18 @@ class _HomeTabState extends State<HomeTab> {
                   ),
                 ),
                 child: AddToCartIcon(
+                  key: globalKeyCartItems,
                   icon: Icon(
                     Icons.shopping_cart,
                     color: CustomColors.customSwatchColor,
                   ),
-                  key: globalKeyCartItems,
-                )),
-          )
+                ),
+              ),
+            ),
+          ),
         ],
       ),
+
       body: AddToCartAnimation(
         gkCart: globalKeyCartItems,
         previewDuration: const Duration(milliseconds: 100),
@@ -68,14 +93,18 @@ class _HomeTabState extends State<HomeTab> {
         },
         child: Column(
           children: [
+            // Campo de pesquisa
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
               child: TextFormField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Colors.white,
                   isDense: true,
-                  hintText: 'Pesquise aqui',
+                  hintText: 'Pesquise aqui...',
                   hintStyle: TextStyle(
                     color: Colors.grey.shade400,
                     fontSize: 14,
@@ -86,58 +115,95 @@ class _HomeTabState extends State<HomeTab> {
                     size: 21,
                   ),
                   border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(60),
-                      borderSide: const BorderSide(
-                        width: 0,
-                        style: BorderStyle.none,
-                      )),
+                    borderRadius: BorderRadius.circular(60),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
                 ),
               ),
             ),
+
+            // Categorias
             Container(
+              padding: const EdgeInsets.only(left: 25),
               height: 40,
-              padding: const EdgeInsets.only(left: 25, right: 25),
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (_, index) {
-                  return CategoryTile(
-                    onPressed: () {
-                      setState(() {
-                        selectedCatrgory = app_data.categories[index];
-                      });
-                    },
-                    category: app_data.categories[index],
-                    isSelected: app_data.categories[index] == selectedCatrgory,
-                  );
-                },
-                separatorBuilder: (_, index) => const SizedBox(
-                  width: 10,
-                ),
-                itemCount: app_data.categories.length,
-              ),
+              child: !isLoading
+                  ? ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (_, index) {
+                        return CategoryTile(
+                          onPressed: () {
+                            setState(() {
+                              selectedCategory = app_data.categories[index];
+                            });
+                          },
+                          category: app_data.categories[index],
+                          isSelected:
+                              app_data.categories[index] == selectedCategory,
+                        );
+                      },
+                      separatorBuilder: (_, index) => const SizedBox(width: 10),
+                      itemCount: app_data.categories.length,
+                    )
+                  : ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: List.generate(
+                        10,
+                        (index) => Container(
+                          alignment: Alignment.center,
+                          margin: const EdgeInsets.only(right: 12),
+                          child: CustomShimmer(
+                            height: 20,
+                            width: 80,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
             ),
+
+            // Grid
             Expanded(
-              child: GridView.builder(
-                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                physics: const BouncingScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                    childAspectRatio: 9 / 11.5),
-                itemCount: app_data.items.length,
-                itemBuilder: (_, index) {
-                  return ItemTile(
-                      item: app_data.items[index],
-                      cartAnimationMethod: itemSelectedCartAnimations);
-                },
-              ),
-            )
+              child: !isLoading
+                  ? GridView.builder(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      physics: const BouncingScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                        childAspectRatio: 9 / 11.5,
+                      ),
+                      itemCount: app_data.items.length,
+                      itemBuilder: (_, index) {
+                        return ItemTile(
+                            item: app_data.items[index],
+                            cartAnimationMethod: itemSelectedCartAnimations);
+                      },
+                    )
+                  : GridView.count(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      physics: const BouncingScrollPhysics(),
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      childAspectRatio: 9 / 11.5,
+                      children: List.generate(
+                        10,
+                        (index) => CustomShimmer(
+                          height: double.infinity,
+                          width: double.infinity,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
--- app/lib/src/pages/splash/splash_screen.dart
+++ app/lib/src/pages/splash/splash_screen.dart
@@ -1,17 +1,63 @@
+import 'package:app/src/config/custom_colors.dart';
+import 'package:app/src/pages/auth/pages/sign_in_screen.dart';
 import 'package:app/src/pages/widgets/app_name_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (c) {
+        return const SignInScreen();
+      }));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Container(
-        child: AppNameWidget(),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            CustomColors.customSwatchColor,
+            CustomColors.customSwatchColor.shade700,
+          ],
+        )),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            AppNameWidget(
+              greenTileColor: Colors.white,
+              isBold: true,
+              textSize: 40,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(
+                Colors.white,
+              ),
+              color: Colors.white,
+            ),
+          ],
+        ),
       ),
     );
   }
